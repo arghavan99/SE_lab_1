424e2ad4cbe8097c63b00af3fd140279036183d6
mahtabbigverdi <bigverdi.1377@gmail.com>
2021-10-18 17:55:01 +0330

    add random forest interface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-
+from Ensembles import *
 def load_data():
     data = load_breast_cancer()
     return train_test_split(data.data, data.target, test_size=0.33, random_state=42)
@@ -16,3 +16,12 @@ if __name__ == '__main__':
           '\n3. SVM')
     cat = int(input())
 
+    if cat == 2:
+        clf = EnsembleAndDTClassifiers(x_train, y_train, x_test, y_test)
+        print('choose among available ensemble models below:'
+              '\n1. Random Forest')
+        num = int(input())
+        if num == 1:
+            print('input number of estimators:')
+            n_estimators = int(input())
+            test_acc = clf.random_forest(n_estimators=n_estimators)
