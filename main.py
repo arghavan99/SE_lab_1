5c5eb092c4484f7c55fdee235605ce6f54cf207a
mahtabbigverdi <bigverdi.1377@gmail.com>
2021-10-18 17:50:52 +0330

    add linear model interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from LinearModel import *
 
 def load_data():
     data = load_breast_cancer()
@@ -15,4 +16,19 @@ if __name__ == '__main__':
           '\n2. EnsembleModels and DecisionTrees'
           '\n3. SVM')
     cat = int(input())
+
+    if cat == 1:
+        clf = LinearClassifiers(x_train, y_train, x_test, y_test)
+        print('choose among available linear models below:'
+              '\n1. Ridge Classifier'
+              '\n2. Logistic Regression')
+        num = int(input())
+        if num == 1:
+            print('input alpha:')
+            alpha = float(input())
+            test_acc = clf.ridge_classifier(alpha)
+        elif num == 2:
+            print('input penalty (l1 or l2 ?):')
+            penalty = input()
+            test_acc = clf.logistic_regression(penalty)
 
